dfb359318f8815fa1b18b32e8a8add69b52dd18a
ilias bhallil <ilias.bhal@gmail.com>
2022-11-04 04:46:04 -0400

    add selectors with cache and cache busting mechanism

--- src/Atom.test.tsx
+++ src/Atom.test.tsx
@@ -1,4 +1,5 @@
-import { Atom } from './Atom';
+import { Atom, AtomSelector } from './Atom';
+import { Entity } from './Entity';
 
 describe('Atom', () => {
   it('can retrieve atom value', () => {
@@ -16,28 +17,29 @@ describe('Atom', () => {
 describe('Atom Selector', () => {
   it('can create a selector with other atoms', () =>{
     const count = Atom.from(3);
-    const double = Atom.select(() => {
-      return count.get() * 2
-    });
+    const double = Atom.select((use) => use(count) * 2);
 
     expect(double.get()).toBe(6);
+  })
+
+  it('throws if trying to manualy set set value', () => {
+    const count = Atom.from(3);
+    const double = Atom.select((use) => use(count) * 2);
+
+    expect(() => double.set(3)).toThrow();
   })
 
   it('can create a selector with other updated atoms', () =>{
     const count = Atom.from(3);
     count.set(4);
-    const double = Atom.select(() => {
-      return count.get() * 2
-    });
+    const double = Atom.select((use) => use(count) * 2);
 
     expect(double.get()).toBe(8);
   })
 
   it('updates selector when base atom is updated', () =>{
     const count = Atom.from(3);
-    const double = Atom.select(() => {
-      return count.get() * 2
-    });
+    const double = Atom.select((use) => use(count) * 2);
 
     count.set(6);
     expect(double.get()).toBe(12);
@@ -48,41 +50,153 @@ describe('Atom Selector', () => {
 
   it('computes selector only when read', () => {
     const count = Atom.from(3);
-    const select = jest.fn();
-    const double = Atom.select(() => {
-      select();
-      return count.get() * 2
-    });
+    const selector = jest.fn().mockImplementation(() => count.get() * 2);
+    const double = Atom.select(selector);
 
-    expect(select).not.toHaveBeenCalled();
+    expect(selector).not.toHaveBeenCalled();
     count.set(6);
     expect(double.get()).toBe(12);
-    expect(select).toHaveBeenCalledTimes(1);
+    expect(selector).toHaveBeenCalledTimes(1);
   })
 
   it('doesn\'t recompute selector if atom hasn\'t changed', () => {
     const count = Atom.from(3);
-    const select = jest.fn();
-    const double = Atom.select(() => {
-      select();
-      return count.get() * 2
-    });
+    const selector = jest.fn().mockImplementation((use) => use(count) * 2);
+    const double = Atom.select(selector);
 
-    expect(select).not.toHaveBeenCalled();
+    expect(selector).not.toHaveBeenCalled();
     
-    double.get()
-    double.get()
+    double.get();
+    double.get();
     double.get()
 
-    expect(select).toHaveBeenCalledTimes(1);
-    select.mockClear();
+    expect(double.get()).toBe(6);
+    expect(selector).toHaveBeenCalledTimes(1);
+    selector.mockClear();
 
     count.set(4);
 
-    double.get();
-    double.get();
-    double.get()
+    expect(selector).not.toHaveBeenCalled();
 
-    expect(select).toHaveBeenCalledTimes(1);
+    double.get();
+    double.get();
+    double.get();
+
+    expect(double.get()).toBe(8);
+    expect(selector).toHaveBeenCalledTimes(1);
   })
+
+  it('accepts a selectors as arguments', () => {
+    const count = Atom.from(3);
+    const selector = jest.fn();
+    const double = Atom.select((use) => {
+      selector();
+      return use(count) * 2;
+    });
+
+    const selector2 = jest.fn();
+    const quad = Atom.select((use) => {
+      selector2();
+      return use(double) * 2;
+    });
+
+    expect(selector).not.toHaveBeenCalled();
+    expect(selector2).not.toHaveBeenCalled();
+
+    expect(quad.get()).toBe(12);
+    expect(selector).toHaveBeenCalledTimes(1);
+    expect(selector2).toHaveBeenCalledTimes(1);
+  })
+
+  it('be up to date when upstream selector isn\'t valid', () => {
+    const count = Atom.from(1);
+    const double = Atom.select((use) => use(count) * 2);
+    const quad = Atom.select((use) => use(double) * 2);
+
+    expect(quad.get()).toBe(4);
+
+    count.set(100)
+
+    expect(quad.get()).toBe(400);
+  })
+
+  it('should recompute invalidated upstream selectors', () => {
+    const count = Atom.from(1);
+    const doubleSpy = jest.fn();
+    const double = Atom.select((use) => {
+      doubleSpy();
+      return use(count) * 2;
+    });
+
+    const tripleSpy = jest.fn();
+    const triple = Atom.select((use) => {
+      tripleSpy();
+      return use(double) * 3;
+    });
+
+    expect(doubleSpy).not.toHaveBeenCalled();
+    expect(tripleSpy).not.toHaveBeenCalled();
+    
+    triple.get();
+    expect(triple.get()).toBe(6);
+    expect(doubleSpy).toHaveBeenCalledTimes(1);
+    expect(tripleSpy).toHaveBeenCalledTimes(1);
+    doubleSpy.mockClear();
+    tripleSpy.mockClear();
+    
+    count.set(100);
+    expect(doubleSpy).not.toHaveBeenCalled();
+    expect(tripleSpy).not.toHaveBeenCalled();
+
+    triple.get()
+    expect(triple.get()).toBe(600);
+    expect(doubleSpy).toHaveBeenCalledTimes(1);
+    expect(tripleSpy).toHaveBeenCalledTimes(1);
+  })
+
+  it('should recompute invalidated upstream selectors tree 2', () => {
+    const count = Atom.from(1);
+    const doubleSpy = jest.fn();
+    const double = Atom.select((use) => {
+      doubleSpy();
+      return use(count) * 2;
+    });
+    
+    const multiple = Atom.from(3);
+    const multiple2 = Atom.from(1000);
+    const variableSpy = jest.fn();
+    const variable = Atom.select((use) => {
+      variableSpy();
+      return use(double) * use(multiple) * use(multiple2);
+    });
+
+    variable.get()
+    expect(variable.get()).toBe(6000);
+    expect(doubleSpy).toHaveBeenCalledTimes(1);
+    expect(variableSpy).toHaveBeenCalledTimes(1);
+    doubleSpy.mockClear();
+    variableSpy.mockClear();
+
+    multiple.set(0)
+    
+    expect(variable.get()).toBe(0);
+    expect(doubleSpy).not.toHaveBeenCalled();
+    expect(variableSpy).toHaveBeenCalledTimes(1);
+    doubleSpy.mockClear();
+    variableSpy.mockClear();
+
+    multiple2.set(5)
+    expect(variable.get()).toBe(0);
+    expect(doubleSpy).not.toHaveBeenCalled();
+    expect(variableSpy).toHaveBeenCalledTimes(1);
+  })
+
+  // This is because we don't get stable references
+  // when working with proxies
+  // we should ensure thet the reference key used
+  // when calling .register .invalidate and .verify is stable
+  // that's why we had to use .selector sometimes
+  // because .selector attribute is not wrapped 
+  // by a Proxy from Entity when read
+  it.todo('remove usage of .selector in DependencyTree');
 })
--- src/Atom.ts
+++ src/Atom.ts
@@ -14,7 +14,7 @@ export class Atom<T> extends Entity {
     return atom;
   }
 
-  static select<V>(selector: () => V) : Atom<V> {
+  static select<V>(selector: (use: <B>(entity: Atom<B>) => B) => V) : AtomSelector<V> {
     const atom = new AtomSelector(selector);
     return atom;
   }
@@ -28,22 +28,79 @@ export class Atom<T> extends Entity {
   }
 }
 
-class AtomSelector<T> extends Atom<T> {
-  protected selector : () => T;
+class DependencyTree {
+  cache = new WeakSet<object>();
+  dependents = new WeakMap<object, Set<object>>();
 
-  constructor(selector: () => T) {
+  register(key: any, dependency: any) {
+    this.cache.add(key.selector);
+
+    if (!this.dependents.has(dependency)) {
+      this.dependents.set(dependency, new Set());
+    }
+
+    this.dependents.get(dependency).add(key);
+  }
+
+  invalidate(key: any) {
+    // remove all caches of selector that have key as dependncy;
+    this.cache.delete(key.selector);
+
+    const dependents = this.dependents.get(key);
+    dependents?.forEach(dependent => {
+      this.invalidate(dependent);
+    });
+  }
+
+  verify(key: any) {
+    return this.cache.has(key.selector);
+  }
+}
+
+export class AtomSelector<T> extends Atom<T> {
+  static tree = new DependencyTree();
+  protected selector : (use: any) => T;
+  constructor(selector: (use: any) => T) {
     super(null);
-
     this.selector = selector;
+  }
+  
+  use = <A>(entity: Atom<A>) : A => {
+    const proxy = Entity.handlePropRead(entity, (parent, prop) => {
+      AtomSelector.tree.register(this, parent);
+
+      // Ensure that get notify when one a dependency is updated
+      // In that case we'll need to invalidate this and downstream selectors
+      Entity.subscribe(parent, (updatedProp) => {
+        if (updatedProp !== prop) return;
+        AtomSelector.tree.invalidate(this);
+      });
+    });
+
+    // We need to return the current atom value
+    if (proxy instanceof Atom) {
+      const value = proxy.get();
+      return value;
+    }
+
+    return proxy;
   }
 
   select() {
-    const value = this.selector();
-    this.set(value);
+    const hasCachedValue = AtomSelector.tree.verify(this);
+    if (!hasCachedValue) {
+      const selected = this.selector(this.use);
+      super.set(selected);
+    } 
+
+    return super.get();
+  }
+
+  set(value: T) {
+    throw new Error('Selector is read only, cannot use .set() method');
   }
 
   get() {
-    this.select();
-    return super.get();
+    return this.select();
   }
 }
