4a1f5d1b620f211eb4967d68b5385dbf231c21f8
ilias bhallil <ilias.bhal@gmail.com>
2022-11-06 12:08:38 -0500

    rename wrong argument name

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -2,7 +2,7 @@ export interface Subscription {
   unsubscribe() : void;
 }
 
-export type SubscriptionHandler = (topic: string | symbol) => void;
+export type SubscriptionHandler = (message: string | symbol) => void;
 
 export class EventBus<Topic extends object> {
   subscriptions = new WeakMap<Topic, Set<SubscriptionHandler>>()
