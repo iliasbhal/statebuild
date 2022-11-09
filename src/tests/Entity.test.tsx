59118030729e40d50297e3a6d2bf44b108e2ced8
ilias bhallil <ilias.bhal@gmail.com>
2022-11-09 12:54:35 -0500

    add test cases

--- src/tests/Entity.test.tsx
+++ src/tests/Entity.test.tsx
@@ -35,4 +35,54 @@ describe('Entity', () => {
       list: ['1'],
     });
   })
+
+  class Worker extends Person {
+    job: string = 'unemployed';
+  }
+
+  it('inherited: should track when object properties are set', () => {
+    const worker = new Worker();
+
+    const callback = jest.fn();
+    Entity.subscribe(worker, callback);
+
+    worker.name = 'Dave';
+
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith('name');
+    expect(worker).toMatchObject({ name: 'Dave' });
+  })
+
+  it('inherited: should track when array is mutated', () => {
+    const worker = new Worker();
+
+    const callback = jest.fn();
+    Entity.subscribe(worker.list, callback);
+
+    worker.list.push('1')
+
+    expect(callback).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalledWith('0');
+    expect(callback).toHaveBeenCalledWith('length');
+    expect(worker).toMatchObject({
+      name: 'John',
+      list: ['1'],
+    });
+  })
+
+  it('inherited: should track worker attributes', () => {
+    const worker = new Worker();
+
+    const callback = jest.fn();
+    Entity.subscribe(worker, callback);
+
+    worker.job = 'baker';
+
+    expect(callback).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalledWith('job');
+    expect(worker).toMatchObject({
+      name: 'John',
+      job: 'baker',
+    });
+  })
 })
