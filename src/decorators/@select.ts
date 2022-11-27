b668a48d7cec51540a1aa7d10acb014db2fab580
ilias bhallil <ilias.bhal@gmail.com>
2022-11-27 00:51:42 -0500

    select decorator can also be used with methods

--- src/decorators/@select.ts
+++ src/decorators/@select.ts
@@ -1,15 +1,36 @@
 import { State } from '../models';
 
 export const select : MethodDecorator  = (desc: any) => {
-  const initialGetter = desc.descriptor.get;
   const selectorByInstance = new WeakMap();
-
-  desc.descriptor.get = function() {
-    if (!selectorByInstance.has(this)) {
-      selectorByInstance.set(this, State.select(initialGetter.bind(this)));
+  const getOrCreateSelector = (instance, initialCallback) => {
+    if (!selectorByInstance.has(instance)) {
+      selectorByInstance.set(instance, State.select(initialCallback.bind(instance)));
     }
 
-    const selector = selectorByInstance.get(this);
-    return selector.get();
+    const selector = selectorByInstance.get(instance);
+    return selector;
   }
+  
+  const { descriptor } = desc 
+  const isGetter = !!descriptor.get;
+  if (isGetter) {
+    const inital = descriptor.get;
+    descriptor.get = function() {  
+      const selector = getOrCreateSelector(this, inital);
+      return selector.get();
+    }
+  }
+
+  const isMethod = typeof descriptor.value === 'function';
+  if (isMethod) {
+    const initial = descriptor.value;
+    descriptor.value = function(...args) {  
+      const selector = getOrCreateSelector(this, initial);
+      return selector.get(...args);
+    }
+  }
+}
+
+export const memo : MethodDecorator = (desc: any) => {
+
 }
--- src/decorators/tests/@select.test.ts
+++ src/decorators/tests/@select.test.ts
@@ -4,6 +4,7 @@ import { select } from '../@select';
 describe('@select', () => {
   const fullNameSpy = jest.fn();
   const reverseFullNameSpy = jest.fn();
+  const methodSpy = jest.fn();
 
   class Person extends State {
     fistName: string = '';
@@ -24,6 +25,11 @@ describe('@select', () => {
     @select get reverseFullName() {
       reverseFullNameSpy();
       return this.lastName + ' ' + this.fistName;
+    }
+
+    @select checkNameWith(testName: string) {
+      methodSpy(testName);
+      return this.lastName + " " + testName;
     }
   }
 
@@ -47,7 +53,7 @@ describe('@select', () => {
     expect(fullNameSpy).toHaveBeenCalledTimes(1);
   })
 
-  it('should recall getter if dependencies have changed updated', () => {
+  it('should invalidate cache if dependencies have changed updated', () => {
     const person = new Person({ fistName: 'John', lastName: 'Doe' });
     expect(person.fullName).toBe('John Doe');
     expect(fullNameSpy).toHaveBeenCalledTimes(1);
@@ -80,5 +86,36 @@ describe('@select', () => {
     expect(person.reverseFullName).toBe('Doe Dave');
     expect(fullNameSpy).toHaveBeenCalledTimes(1);
     expect(reverseFullNameSpy).toHaveBeenCalledTimes(1);
+  })
+
+  it('should memoize decorated methods', () => {
+    const person = new Person({ fistName: 'John', lastName: 'Doe' });
+
+    person.checkNameWith('The 2nd');
+    expect(person.checkNameWith('The 2nd')).toBe('Doe The 2nd');
+    expect(methodSpy).toHaveBeenCalledTimes(1);
+
+    person.checkNameWith('The 3rd');
+    expect(person.checkNameWith('The 3rd')).toBe('Doe The 3rd');
+
+    expect(person.checkNameWith('The 2nd')).toBe('Doe The 2nd');
+    expect(methodSpy).toHaveBeenCalledTimes(2);
+    expect(methodSpy).toHaveBeenLastCalledWith('The 3rd');
+    methodSpy.mockClear();
+
+    person.lastName = 'Found';
+    expect(person.checkNameWith('The 3rd')).toBe('Found The 3rd');
+    person.checkNameWith('The 3rd');
+    expect(methodSpy).toHaveBeenCalledTimes(1);
+    methodSpy.mockClear();
+
+    expect(person.checkNameWith('The 2nd')).toBe('Found The 2nd');
+    expect(person.checkNameWith('The 2nd')).toBe('Found The 2nd');
+    expect(methodSpy).toHaveBeenCalledTimes(1);
+    expect(methodSpy).toHaveBeenLastCalledWith('The 2nd');
+    methodSpy.mockClear();
+
+    expect(person.checkNameWith('The 3rd')).toBe('Found The 3rd');
+    expect(methodSpy).not.toHaveBeenCalled();
   })
 });
