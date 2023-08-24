e8195d096ab26fa63edab3314b976c3e3669dfcd
ilias bhallil <ilias.bhal@gmail.com>
2023-08-25 00:01:16 +0700

    feature: useSelector should accept args

--- src/hooks/useSelector.ts
+++ src/hooks/useSelector.ts
@@ -3,8 +3,17 @@ import { Selector, SelectorCallback } from '../models';
 
 export const useSelector = <Fn extends SelectorCallback>(selector: Selector<Fn>, ...args: unknown[]) : ReturnType<Fn> => {
   const [value, setValue] = React.useState(() => selector.get(...args));
+  const renderCount = React.useRef(0);
 
   React.useEffect(() => {
+    const isFirstRender = renderCount.current === 0;
+    if (!isFirstRender) {
+      const nextValue = selector.get(...args);
+      setValue(nextValue);
+    } else {
+      renderCount.current += 1;
+    }
+
     const subscription = Selector.subsribeToSelectorChanges(selector, () => {
       setTimeout(() => {
         const nextValue = selector.get(...args);
@@ -15,7 +24,7 @@ export const useSelector = <Fn extends SelectorCallback>(selector: Selector<Fn>,
     return () => {
       subscription.unsubscribe()
     }
-  },[]);
+  }, [...args]);
 
   return value;
 }
