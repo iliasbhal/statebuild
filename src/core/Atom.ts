cc84fe0f3ab401b40877e21fd1cf330f607f87ff
ilias bhallil <ilias.bhal@gmail.com>
2025-02-03 14:40:07 +0700

    update: atoms are now tracking their promise state

--- src/core/Atom.ts
+++ src/core/Atom.ts
@@ -1,12 +1,36 @@
 import { Entity } from './base/Entity';
 
+type SyncPromiseData<T> = {
+  promise: Promise<T>
+  isLoading: boolean,
+  value: Awaited<T>,
+  error: Error,
+};
+
+class AtomWaiting<T> extends Error {
+  promise: Promise<T>
+
+  constructor(promise: Promise<T>) {
+    super("Waiting!");
+    this.promise = promise;
+  }
+}
+
 export class Atom<T> {
-  atom: { value: T };
+  static Waiting = AtomWaiting;
+
+  atom: {
+    value: T,
+    syncValue?: SyncPromiseData<T>
+  };
 
   constructor(value: T) {
     this.atom = Entity.wrap({
       value,
+      syncValue: this.toSyncValue(value)
     });
+
+    this.trackSyncValue(value);
   }
 
   get() {
@@ -14,7 +38,63 @@ export class Atom<T> {
   }
 
   set(value: T) {
-    this.atom.value = value
+    this.atom.value = value;
+    this.atom.syncValue = this.toSyncValue(value);
+    this.trackSyncValue(value);
+  }
+
+  getAsync() {
+    return this.atom.syncValue
+  }
+
+  getSync(): Awaited<T> {
+    if (this.atom.syncValue.isLoading) {
+      throw new Atom.Waiting(this.atom.syncValue.promise);
+    }
+
+    return this.atom.syncValue?.value;
+  }
+
+  private currentPromise: Promise<T> | T = null
+  private trackSyncValue(promise: Promise<T> | T) {
+    const isPromise = promise instanceof Promise
+    this.currentPromise = promise;
+
+    if (isPromise) {
+      const updateSyncData = (data, error) => {
+        const hasChanged = this.currentPromise !== promise;
+        if (hasChanged) return;
+
+        Object.assign(this.atom.syncValue, {
+          value: data || null,
+          error: error || null,
+          isLoading: false,
+        });
+      }
+
+      promise.then((data) => updateSyncData(data, null))
+        .catch((error) => updateSyncData(null, error));
+    }
+  }
+
+  private toSyncValue(promise: Promise<T> | T) {
+    const isPromise = promise instanceof Promise
+
+    if (isPromise) {
+      return {
+        promise: promise,
+        isLoading: true,
+        value: null,
+        error: null,
+      }
+    }
+
+    return {
+      promise: null,
+      isLoading: false,
+      value: promise as Awaited<T>,
+      error: null,
+    }
   }
 
   dispose() {
--- src/core/tests/Atom.test.tsx
+++ src/core/tests/Atom.test.tsx
@@ -1,4 +1,5 @@
-import { State, } from '../';
+import wait from 'wait';
+import { Atom, State, } from '../';
 
 describe('Atom', () => {
   it('can retrieve atom value', () => {
@@ -18,5 +19,68 @@ describe('Atom', () => {
 
     atom.set(4);
     expect(atom()).toBe(4);
+  })
+
+  it('can read the state of a promise (non-blocking)', async () => {
+    const atom  = State.from(new Promise<number>(r => setTimeout(() => r(100), 100)));
+    
+    {
+      const { isLoading, value, error } = atom.getAsync();
+      expect(isLoading).toBe(true);
+      expect(value).toBe(null);
+      expect(error).toBe(null);
+    }
+
+    await wait(100);
+
+    {
+      const { isLoading, value, error } = atom.getAsync();
+      expect(isLoading).toBe(false);
+      expect(value).toBe(100);
+      expect(error).toBe(null);
+    }
+
+  })
+
+  it('can read the state of a promise (blocking - waterfall)', async () => {
+    const createPromise = (value: any, timeout: number) => new Promise<number>(r => setTimeout(() => r(value), timeout));
+    const atom  = State.from(createPromise('First', 100));
+    
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+
+    await wait(100);
+
+    expect(atom.getSync()).toBe('First');
+
+    atom.set(createPromise('Second', 100));
+
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+
+    await wait(100);
+
+    expect(atom.getSync()).toBe('Second');
+  })
+
+  it('can read the state of a promise (blocking - race)', async () => {
+    const createPromise = (value: any, timeout: number) => new Promise<number>(r => setTimeout(() => r(value), timeout));
+    const atom  = State.from(createPromise('First', 100));
+    
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+
+    await wait(50);
+
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+    
+    atom.set(createPromise('Second', 100));
+
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+    
+    await wait(50);
+
+    expect(() => atom.getSync()).toThrow(Atom.Waiting);
+
+    await wait(50);
+
+    expect(atom.getSync()).toBe('Second');
   })
 })
